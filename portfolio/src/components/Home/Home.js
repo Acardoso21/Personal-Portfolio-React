f43256c45434a66de519f6dc0b9bfe3e32290b12
Andre <acardoso.ny@gmail.com>
2024-10-11 13:46:17 +0100

    deleting everything and then adding it back in

--- portfolio/src/components/Home/Home.js
+++ portfolio/src/components/Home/Home.js
@@ -1,5 +1,6 @@
+// src/components/Home/Home.js
 import React from 'react';
-import { Typed } from 'react-typed';
+import { Typed } from 'react-typed'; // Ensure this is installed
 import './Home.css';
 
 function Home() {
@@ -9,10 +10,10 @@ function Home() {
         <h1>Welcome to My Portfolio</h1>
         <p>
           Hi, I'm [Your Name], a passionate <Typed
-            strings={["Web Developer", "Designer", "Content Creator"]}
-            typeSpeed={40}
-            backSpeed={50}
-            loop
+            strings={["Developer", "Designer", "Creator"]} // Place your Typed component here
+            typeSpeed={50} // Speed of typing
+            backSpeed={70} // Speed of backspacing
+            loop // Loop the typing
           />.
         </p>
         <a href="#projects" className="cta-button">View My Work</a>
